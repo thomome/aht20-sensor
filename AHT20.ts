ffc1aaab65bded025bf9f7c05ae43e42d5acfc45
Gakuto1112 <gakuto131112@gmail.com>
2023-10-08 12:44:26 +0900

    add JSDoc

--- AHT20.ts
+++ AHT20.ts
@@ -14,13 +14,29 @@ const AHT20_CMD_MEASURE: number[] = [0xAC, 0x33, 0x00]
 const AHT20_STATUS_BUSY: number = 0x80;
 const AHT20_STATUS_CALIBRATED: number = 0x08;
 
+/**
+ * AHT20 sensor class with data read functions.
+ */
 export default class AHT20 {
+    /**
+     * Bus instance.
+     */
     private readonly bus: i2c.PromisifiedBus;
 
+    /**
+     * Constructor
+     * @param bus bus instance
+     */
 	constructor(bus: i2c.PromisifiedBus) {
 		this.bus = bus;
 	}
 
+    /**
+     * Opens i2c bus and connect to the AHT20 sensor.
+     * @param busNumber Target bus number to open. Default is 1.
+     * @returns AHT20 instance with opened bus instance. You can read information with this instance.
+     * @throws An error that occurred while opening i2c bus.
+     */
 	static async open(busNumber: number = 1): Promise<AHT20> {
 		try {
 			const bus: i2c.PromisifiedBus = await i2c.openPromisified(busNumber);
@@ -32,6 +48,11 @@ export default class AHT20 {
 		}
 	}
 
+    /**
+     * Initializes AHT20 sensor.
+     * @returns `true` if successfully initialized the sensor.
+     * @throws An error that occurred while initializing the sensor.
+     */
 	async init(): Promise<boolean> {
 		try {
 			await sleep(20);
@@ -46,6 +67,11 @@ export default class AHT20 {
 		}
 	}
 
+    /**
+     * Gets AHT20 sensor status.
+     * @returns Sensor status
+     * @throws An error that occurred while getting sensor status.
+     */
 	async getStatus(): Promise<number> {
 		try {
 			const buf: Buffer = Buffer.alloc(1);
@@ -56,6 +82,11 @@ export default class AHT20 {
 		}
 	}
 
+    /**
+     * Resets AHT20 sensor.
+     * @returns `true` if successfully reset the sensor.
+     * @throws An error that occurred while resetting the sensor.
+     */
 	async reset(): Promise<boolean> {
 		try {
 			const buf: Buffer = Buffer.from(AHT20_CMD_SOFTRESET);
@@ -67,6 +98,11 @@ export default class AHT20 {
 		}
 	}
 
+    /**
+     * Calibrates AHT20 sensor.
+     * @returns `true` if successfully calibrated the sensor.
+     * @throws An error that occurred while calibrating the sensor.
+     */
 	async calibrate(): Promise<boolean> {
 		try {
 			const buf: Buffer = Buffer.from(AHT20_CMD_CALIBRATE);
@@ -84,6 +120,11 @@ export default class AHT20 {
 		}
 	}
 
+    /**
+     * Reads information from AHT20 sensor.
+     * @returns Information dictionary with temperature and humidity data.
+     * @throws An error that occurred while Reading the information.
+     */
 	async readData(): Promise<{[key: string]: number}> {
 		try {
 			const buf: Buffer = Buffer.from(AHT20_CMD_MEASURE);
@@ -108,6 +149,11 @@ export default class AHT20 {
 		}
 	}
 
+    /**
+     * Gets temperature data from AHT20 sensor.
+     * @returns Temperature gotten from the sensor in Celsius.
+     * @throws An error that occurred while getting temperature data.
+     */
 	async temperature(): Promise<number> {
 		try {
 			const { temperature }: {[key: string]: number} = await this.readData();
@@ -117,6 +163,11 @@ export default class AHT20 {
 		}
 	}
 
+    /**
+     * Gets humidity data from AHT20 sensor.
+     * @returns Humidity gotten from the sensor in RH%.
+     * @throws An error that occurred while getting humidity data.
+     */
 	async humidity(): Promise<number> {
 		try {
 			const { humidity }: {[key: string]: number} = await this.readData();
